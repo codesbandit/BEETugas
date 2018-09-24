46c51f4664a1118250e2ae7894512be125776d76
Codesbandit <abdillah.pandu0@gmail.com>
2018-09-24 09:40:31 +0700

    Make ascending array/list python

---
+++ ascending.py
@@ -0,0 +1,12 @@
+arr = ['A1574','A6278','B3689','A8965','B6793','A4555','C6384']
+
+#Ascending Sort List
+arr.sort()
+
+#Descending Sort List
+# arr.sort(reverse=True)
+
+#Descending Order 2
+# sorted(arr, reverse=True)
+
+print(arr)
